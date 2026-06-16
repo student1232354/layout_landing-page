3bfc9839de3aba42b115efaf77c8fa0c5b6107a4
student1232354 <tarasromanenko33@gmail.com>
2026-06-16 12:09:17 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,1 @@
 'use strict';
-
-const burgerBtn = document.querySelector('.header__menu');
-const burgerMenu = document.querySelector('.burger__menu');
-const burgercross = document.querySelector('.burger__cross');
-
-burgerBtn.addEventListener('click', () => {
-  burgerMenu.classList.toggle('burger__menu--open');
-});
-
-burgercross.addEventListener('click', () => {
-  burgerMenu.classList.toggle('burger__menu--open');
-});
